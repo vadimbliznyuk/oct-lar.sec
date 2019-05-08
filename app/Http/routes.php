7aada108fe027995c8d0d0e9b618768137bddca8
Vasyl <admin@gmail.com>
2019-05-08 19:41:51 +0300

    save task and errors

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,13 +1,26 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use App\Task;
 
 Route::get('/', function () {
     return view('tasks.index'); // уроки это вид tasks
 });
 
-Route::post('/tasks', function (Request $request){
+Route::post('/tasks', function (Request $request) {
+
+    $validator = Validator::make($request->all(), [
+		'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+	return redirect('/')
+			->withInput()
+			->withErrors($validator);
+    }
     
-    
+    $task = new Task();
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
 });
