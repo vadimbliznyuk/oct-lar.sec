ae96987c2062f145aa0f04b66f49a79ac8ee3bdc
vadimbliznyuk <vadim.bliznyuk86@gmail.com>
2019-05-10 19:28:52 +0300

    edit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,46 +6,55 @@ use App\Task;
 Route::get('/', function () {
     $tasks = Task::all();
 
-//    var_dump($tasks);
-//    exit();
-
     return view('tasks.index', [
-	'tasks' => $tasks,
-    ]); // уроки это вид tasks
+        'tasks' => $tasks,
+    ]); 
 });
 
 Route::post('/tasks', function (Request $request) {
 
     $validator = Validator::make($request->all(), [
-		'name' => 'required|max:255',
+                'name' => 'required|max:255',
     ]);
 
     if ($validator->fails()) {
-	return redirect('/')
-			->withInput()
-			->withErrors($validator);
+        return redirect('/')
+                        ->withInput()
+                        ->withErrors($validator);
     }
 
     $task = new Task();
     $task->name = $request->name;
+
     $task->save();
     return redirect('/');
 });
 
-Route::delete('/tasks/{task}', function (Task $task){
-    
+Route::delete('/tasks/{task}', function (Task $task) {
     $task->delete();
     return redirect('/');
 });
 
-Route::get('/tasks/{task}', function (Task $task) {
-    
-//    var_dump('sdf');
-//    die();
-    
+Route::get('/tasks/{task}/edit', function (Task $task) {
+
     return view('tasks.edit', [
-//	'tasks' => $tasks,
-    ]); // уроки это вид tasks
-    
-    
+        'task' => $task,
+    ]); 
+});
+
+Route::patch('/tasks/{task}', function (Request $request, Task $task ) {
+
+    $validator = Validator::make($request->all(), [
+                'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return back()
+                ->withInput()
+                ->withErrors($validator);
+    }
+
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
 });
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,1 +1,32 @@
+@extends('layouts.app')
+@section('content')
+<!-- Bootstrap шаблон... -->
 
+<div class="panel-body">
+    <!-- Отображение ошибок проверки ввода -->
+    @include('common.errors')
+
+    <!-- Форма новой задачи -->
+    <form action="/tasks/{{$task->id}}" method="POST" class="form-horizontal">
+	{{ csrf_field() }}
+        {{method_field('PATCH')}}
+        
+	<!-- Имя задачи -->
+	<div class="form-group">
+	    <label for="task" class="col-sm-3 control-label">Задача</label>
+
+	    <div class="col-sm-6">
+                <input type="text" name="name" id="task-name" class="form-control" value="<?=$task->name?>">
+	    </div>
+	</div>
+
+	<!-- Кнопка изменения задачи -->
+	<div class="form-group">
+	    <div class="col-sm-offset-3 col-sm-6">
+		<button type="submit" class="btn btn-primary">
+		    <i class="fa fa-plus btn-primary"></i> Изменить задачу
+		</button>
+	    </div>
+	</div>
+    </form>
+</div>
